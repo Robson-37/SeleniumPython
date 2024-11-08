66a308d64fa62a266e1a4746d23c5e1503a1fb44
Robson Luiz <robson.luizsilva2019@gmail.com>
2024-11-08 14:56:16 -0300

    Criação da pasta

---
+++ Automacao/src/test/java/qa/selenium/python/Automacao/AutomacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.selenium.python.Automacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
